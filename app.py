6a1de477f580e4690e809ae14ecdc3007df00f14
Git-Shubham14 <shubhambsatpute1407@gmail.com>
2026-05-07 19:54:08 +0530

    updated app meassage

--- app.py
+++ app.py
@@ -2,12 +2,60 @@ from flask import Flask
 
 app = Flask(__name__)
 
+style = """
+<style>
+    body{
+        font-family: Arial;
+        text-align:center;
+        margin-top:100px;
+    }
+
+    .box{
+        width:300px;
+        margin:auto;
+        padding:20px;
+        border:1px solid #ccc;
+        border-radius:8px;
+    }
+
+    a{
+        text-decoration:none;
+        margin:5px;
+        color:black;
+    }
+</style>
+"""
+
+menu = """
+<br><br>
+<a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/contact">Contact</a>
+"""
+
 @app.route("/")
 def home():
-    return """
-    <h1>Hello Cloud</h1>
-    <p>My web app is deployed using PaaS (Render)</p>
-    """
+    return style + """
+    <div class="box">
+        <h2>Home</h2>
+        <p>Simple Flask App</p>
+    """ + menu + "</div>"
+
+@app.route("/about")
+def about():
+    return style + """
+    <div class="box">
+        <h2>About</h2>
+        <p>About Page</p>
+    """ + menu + "</div>"
+
+@app.route("/contact")
+def contact():
+    return style + """
+    <div class="box">
+        <h2>Contact</h2>
+        <p>demo@gmail.com</p>
+    """ + menu + "</div>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=3000,debug=True)
